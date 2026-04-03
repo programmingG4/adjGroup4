8315331a27ed59650d5c83bead267b3125d49927
김현정 <sammy030301@gmail.com>
2026-04-03 23:06:52 +0900

    first commit

---
+++ src/main/java/kr/ac/dankook/campuson/CampusonApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.dankook.campuson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/dankook/campuson/CampusonApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.dankook.campuson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
